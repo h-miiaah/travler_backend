f669782642914db2591db26d2ab2fc9e574e169f
h-miiaah <haim.miiaah5@gmail.com>
2020-07-11 21:17:13 -0400

    destination model, migration, seed data added

---
+++ app/models/destination.rb
@@ -0,0 +1,3 @@
+class Destination < ApplicationRecord
+    # has_many :locations
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+maldives = Destination.create(name: "Maldives")
+tahiti = Destination.create(name: "Tahiti")
+fiji = Destination.create(name: "Fiji")
