d7b3144c3e06434577d5792012944c910ce5b543
h-miiaah <haim.miiaah5@gmail.com>
2020-07-18 15:35:04 -0400

    added index route to destinations controller

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::DestinationsController < ApplicationController
+    def index
+        destinations = Destination.all
+        render json: DestinationSerializer.new(destinations)
+    end
 end
