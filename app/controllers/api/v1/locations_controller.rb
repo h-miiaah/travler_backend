a96fe49c31eb9fcd80704d97113dddfbdcf7a46a
h-miiaah <haim.miiaah5@gmail.com>
2020-07-18 14:50:33 -0400

    added create route to locations controller and added create route

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::LocationsController < ApplicationController
     def index
-        @locations = Location.all
-        render json: @locations
+        locations = Location.all
+        render json: locations
+    end
+
+    def create
+        location = Location.new(location_params)
+        if location.save
+            render json: location, status: :accepted
+        else
+            render json: {errors: location.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:name, :description, :image_url, :destination_id)
     end
 end
