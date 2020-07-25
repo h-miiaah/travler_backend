b7f19972cd1237f2d964f94ba320125d2855118c
h-miiaah <haim.miiaah5@gmail.com>
2020-07-24 22:32:28 -0400

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,12 @@
 Destination.destroy_all
 Location.destroy_all
 
-maldives = Destination.create(name: "Maldives")
-tahiti = Destination.create(name: "Tahiti")
-fiji = Destination.create(name: "Fiji")
+tropical = Destination.create(name: "Tropical")
+urban = Destination.create(name: "Urban")
+city = Destination.create(name: "City")
+nature = Destination.create(name: "Nature")
+adventurous = Destination.create(name: "Adventurous")
 
-Location.create(name:"Male'", description:"Male' is the capital city, but most visitors head straight out to one of the remote atolls, where luxury resorts and bone-white beaches fringed by aquamarine waters await. Diving and snorkeling are world-class, and the Maldives is also a top surfing destination with the most popular surf spots in the North and South Male' Atolls.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-maldives.jpg", destination_id: maldives.id)
-Location.create(name:"Bora Bora", description:"Bora Bora is the quintessential South Pacific paradise. This lush and dramatically beautiful island in French Polynesia rises to a sharp emerald peak ringed by an azure lagoon. Clusters of coconut palms bristle along the beaches, and luxury bungalows perch over the crystal-clear waters, some with glass floor panels, so you can peer into the thriving sea below.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-bora-bora.jpg", destination_id: tahiti.id)
-Location.create(name:"Mamanuca Islands", description:"A short boat ride from the gateway town of Nadi, this picture-perfect string of about 20 islands are popular for their gleaming palm-studded beaches, crystal-clear waters, and thriving coral reefs.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-mamanuca-islands.jpg", destination_id: fiji.id)
+Location.create(name:"Male'", description:"Male' is the capital city, but most visitors head straight out to one of the remote atolls, where luxury resorts and bone-white beaches fringed by aquamarine waters await. Diving and snorkeling are world-class, and the Maldives is also a top surfing destination with the most popular surf spots in the North and South Male' Atolls.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-maldives.jpg", destination_id: tropical.id)
+Location.create(name:"Bora Bora", description:"Bora Bora is the quintessential South Pacific paradise. This lush and dramatically beautiful island in French Polynesia rises to a sharp emerald peak ringed by an azure lagoon. Clusters of coconut palms bristle along the beaches, and luxury bungalows perch over the crystal-clear waters, some with glass floor panels, so you can peer into the thriving sea below.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-bora-bora.jpg", destination_id: tropical.id)
+Location.create(name:"Mamanuca Islands", description:"A short boat ride from the gateway town of Nadi, this picture-perfect string of about 20 islands are popular for their gleaming palm-studded beaches, crystal-clear waters, and thriving coral reefs.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-mamanuca-islands.jpg", destination_id: tropical.id)
