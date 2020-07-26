97b41abfb9a474397b1c9e6cc0d52fcfa83b09f7
h-miiaah <haim.miiaah5@gmail.com>
2020-07-25 20:05:19 -0400

    updated db and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,12 @@
 Destination.destroy_all
 Location.destroy_all
 
-tropical = Destination.create(name: "Tropical")
-urban = Destination.create(name: "Urban")
+adventurous = Destination.create(name: "Adventurous")
 city = Destination.create(name: "City")
 nature = Destination.create(name: "Nature")
-adventurous = Destination.create(name: "Adventurous")
+random = Destination.create(name: "Random")
+tropical = Destination.create(name: "Tropical")
+urban = Destination.create(name: "Urban")
 
 Location.create(name:"Male'", description:"Male' is the capital city, but most visitors head straight out to one of the remote atolls, where luxury resorts and bone-white beaches fringed by aquamarine waters await. Diving and snorkeling are world-class, and the Maldives is also a top surfing destination with the most popular surf spots in the North and South Male' Atolls.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-maldives.jpg", destination_id: tropical.id)
 Location.create(name:"Bora Bora", description:"Bora Bora is the quintessential South Pacific paradise. This lush and dramatically beautiful island in French Polynesia rises to a sharp emerald peak ringed by an azure lagoon. Clusters of coconut palms bristle along the beaches, and luxury bungalows perch over the crystal-clear waters, some with glass floor panels, so you can peer into the thriving sea below.", image_url:"https://www.planetware.com/photos-large/SEY/best-tropical-vacations-bora-bora.jpg", destination_id: tropical.id)
